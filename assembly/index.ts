c6fced7b53ba51891e56b7496f6fb20e75460f3c
Cristian Zambrano <cristian.zambrano@cloudmex.io>
2021-06-02 14:13:43 -0600

    Inlude methods to create and request complaints tickets from a near smart contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * this smart contracts saves tickets that an instituion can take to solve them
  * anyone can make a ticket
  * the ticket is solved if the sender of the ticker accepts it like done
@@ -7,22 +7,36 @@
  *
  */
 
-import {PersistentVector,Context } from "near-sdk-as"
-import { Ticket,categories,Loc} from "./models"
-
-
-let tickets= new PersistentVector<Ticket>("tick")
+import { Context } from "near-sdk-as";
+import { complaints, CitizenComplaint, categories } from "./models";
 
 /**
  * this functions allows us to create new tickets
  * @param title what should be displayed at the card on the front
  * @param category what topic is covering
- * @param location where is the location of the problem 
+ * @param location where is the location of the problem
  * @todo avoid ticket redundacy
  */
-export function addNewTicket(title:string,category:categories,location:Loc):void {
-
-  tickets.push(new Ticket("firstT","there is a light that dont work properly",category,location,Context.sender))
-
+export function addNewComplaint(
+  title: string,
+  description: string,
+  category: categories,
+  location: string
+): void {
+  complaints.push(
+    new CitizenComplaint(title, description, category, location, Context.sender)
+  );
 }
 
+export function getComplaints(): CitizenComplaint[] {
+  const numTickets = complaints.length;
+  const result = new Array<CitizenComplaint>(numTickets);
+  for (let i = 0; i < numTickets; i++) {
+    result[i] = complaints[i];
+  }
+  return result;
+}
+
+export function numberOfTickets(): u64 {
+  return complaints.length;
+}
